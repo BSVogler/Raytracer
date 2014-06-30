fda3113dcf48d4606dcf9ef99e250fa644290585
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-06-30 12:39:35 +0200

    Add renderer skeleton.

---
+++ framework/renderer.cpp
@@ -0,0 +1,44 @@
+#include "renderer.hpp"
+
+Renderer::Renderer(unsigned w, unsigned h, std::string const& file)
+  : width_(w)
+  , height_(h)
+  , colorbuffer_(w*h, Color(0.0, 0.0, 0.0))
+  , filename_(file)
+  , ppm_(width_, height_)
+{}
+
+void Renderer::render()
+{
+  const std::size_t checkersize = 20;
+
+  for (unsigned y = 0; y < height_; ++y) {
+    for (unsigned x = 0; x < width_; ++x) {
+      Pixel p(x,y);
+      if ( ((x/checkersize)%2) != ((y/checkersize)%2)) {
+        p.color = Color(0.0, 1.0, float(x)/height_);
+      } else {
+        p.color = Color(1.0, 0.0, float(y)/width_);
+      }
+
+      write(p);
+    }
+  }
+  ppm_.save(filename_);
+}
+
+void Renderer::write(Pixel const& p)
+{
+  // flip pixels, because of opengl glDrawPixels
+  size_t buf_pos = (width_*p.y + p.x);
+  if (buf_pos >= colorbuffer_.size() || (int)buf_pos < 0) {
+    std::cerr << "Fatal Error Renderer::write(Pixel p) : "
+      << "pixel out of ppm_ : "
+      << (int)p.x << "," << (int)p.y
+      << std::endl;
+  } else {
+    colorbuffer_[buf_pos] = p.color;
+  }
+
+  ppm_.write(p);
+}
---
+++ framework/renderer.hpp
@@ -0,0 +1,31 @@
+#ifndef BUW_RENDERER_HPP
+#define BUW_RENDERER_HPP
+
+#include "color.hpp"
+#include "pixel.hpp"
+#include "ppmwriter.hpp"
+#include <string>
+#include <glm/glm.hpp>
+
+class Renderer
+{
+public:
+  Renderer(unsigned w, unsigned h, std::string const& file);
+
+  void render();
+  void write(Pixel const& p);
+
+  inline std::vector<Color> const& colorbuffer() const
+  {
+    return colorbuffer_;
+  }
+
+private:
+  unsigned width_;
+  unsigned height_;
+  std::vector<Color> colorbuffer_;
+  std::string filename_;
+  PpmWriter ppm_;
+};
+
+#endif // #ifndef BUW_RENDERER_HPP
