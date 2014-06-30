c34183a61fbd32e6fcfd01b7309d42e8c5ba41bb
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-06-30 10:17:59 +0200

    Add operators.
    operator+=
    operator-=
    operator+
    operator-
    operator<<

--- framework/color.hpp
+++ framework/color.hpp
@@ -1,12 +1,50 @@
 #ifndef BUW_COLOR_HPP
 #define BUW_COLOR_HPP
 
+#include <iostream>
+
 struct Color
 {
-  Color(float r, float g, float b) : r_(r), g_(g), b_(b) {}
+  Color(float red, float green, float blue) : r_(red), g_(green), b_(blue) {}
   float r_;
   float g_;
   float b_;
+
+  friend std::ostream& operator<<(std::ostream& os, Color const& c)
+  {
+    os << "(" << c.r_ << "," << c.g_ << "," << c.b_ << ")\n";
+    return os;
+  }
+
+  Color& operator+=(Color const& other)
+  {
+    r_ += other.r_;
+    g_ += other.g_;
+    b_ += other.b_;
+    return *this;
+  }
+
+  Color& operator-=(Color const& other)
+  {
+    r_ -= other.r_;
+    g_ -= other.g_;
+    b_ -= other.b_;
+    return *this;
+  }
+
+  friend Color operator+(Color const& a, Color const& b)
+  {
+    auto tmp(a);
+    tmp += b;
+    return tmp;
+  }
+
+  friend Color operator-(Color const& a, Color const& b)
+  {
+    auto tmp(a);
+    tmp -= b;
+    return tmp;
+  }
 };
 
 #endif //#define BUW_COLOR_HPP
