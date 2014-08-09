9197711b66f06293a14fa43443396cd089e0bc23
= <e-mail@benediktvogler.de>
2014-08-09 14:49:53 +0200

    added basic camera class

---
+++ framework/Camera.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Camera.cpp
+ * Author: Benedikt Vogler
+ * 
+ * Created on 9. August 2014, 14:49
+ */
+
+#include "Camera.hpp"
+
+Camera::Camera() {
+}
+
+Camera::Camera(const Camera& orig) {
+}
+
+Camera::~Camera() {
+}
+
---
+++ framework/Camera.hpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Camera.hpp
+ * Author: Benedikt Vogler
+ *
+ * Created on 9. August 2014, 14:49
+ */
+
+#ifndef CAMERA_HPP
+#define	CAMERA_HPP
+
+class Camera {
+public:
+    Camera();
+    Camera(const Camera& orig);
+    virtual ~Camera();
+private:
+
+};
+
+#endif	/* CAMERA_HPP */
+
