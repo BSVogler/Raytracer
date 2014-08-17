018e7b16921b56926747d3d4e20c9f8a8f9b05b9
= <e-mail@benediktvogler.de>
2014-08-17 18:56:07 +0200

    Fixes sphere collission

--- framework/Sphere.cpp
+++ framework/Sphere.cpp
@@ -8,21 +8,20 @@
 #include "Sphere.hpp"
 
 std::pair<bool, glm::vec3> Sphere::intersect(Ray const& ray) const {
-    float b = 2 *(glm::dot(ray.origin,ray.direction));
-    float c = glm::dot(ray.origin,ray.origin) - radius*radius;
+    glm::vec3 l = ray.origin - center;
+    float a = glm::dot(ray.direction, ray.direction);
+    float b = 2.0f*(glm::dot(l,ray.direction));
+    float c = glm::dot(l,l) - radius*radius;
     
-    int root = b*b-4*c;
-    if (root>0) {//wenn es min. eine Lösung gibt
-        float t1 = -b-sqrt(root);
-        float t2 = -b+sqrt(root);
+    float det = b*b-4.0f*a*c;
+    if (det>=0.0f) {//wenn es min. eine Lösung gibt
+        float t1 = (-b-sqrt(det))/(2.0f*a);
+        float t2 = (-b+sqrt(det))/(2.0f*a);
         
         if (t1 < t2)//nimm Punkt, welcher näher dran ist
-            return std::make_pair(true, ray.origin+ray.direction*t1);//use t1
+            return std::make_pair(true, l-(ray.origin+ray.direction*t1));//use t1
         else 
-            return std::make_pair(true, ray.origin+ray.direction*t2);//use t2
+            return std::make_pair(true, l-(ray.origin+ray.direction*t2));//use t2
     } else 
         return std::make_pair(false, ray.origin);//keine Lösung
 }
-
-
-
