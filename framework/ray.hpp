77a9c1d3800b55cd5d024963fb62f00abb9c3dfb
= <e-mail@benediktvogler.de>
2014-08-17 15:33:23 +0200

    added output for ray

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -2,6 +2,7 @@
 #define BUW_RAY_HPP
 
 #include <glm/glm.hpp>
+#include <ostream>
 
 /**
  * A reay consists of an origin and a diretion.
@@ -10,6 +11,11 @@ struct Ray
 {
     glm::vec3 origin;
     glm::vec3 direction;
+    
+    friend std::ostream& operator<<(std::ostream& os, Ray const& r){
+        return os<<"("<<r.origin.x<<","<<r.origin.y<<","<<r.origin.z<<")+("<<r.direction.x<<","<<r.direction.y<<","<<r.direction.z<<")";
+    }
+    
 };
 
 #endif
