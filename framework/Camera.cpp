491f69846179910eb1a0d57728cca54245d7e53d
= <e-mail@benediktvogler.de>
2014-08-17 13:51:21 +0200

    added vectors to camera

--- framework/Camera.cpp
+++ framework/Camera.cpp
@@ -6,13 +6,20 @@
  */
 
 #include "Camera.hpp"
+#include "glm/detail/type_vec.hpp"
 
 Camera::Camera():
     name("unnamed"),
-    fovX(100)
+    fovX(100),
+    origin(glm::vec3(0,0,0)),  
+    n(glm::vec3(0,0,-1)),
+    up(glm::vec3(0,1,0))
 {}
 
 Camera::Camera(const std::string name, const int fovX):
     name(name),
-    fovX(fovX)
+    fovX(fovX),
+    origin(glm::vec3(0,0,0)),  
+    n(glm::vec3(0,0,-1)),
+    up(glm::vec3(0,1,0))
 {}
--- framework/Camera.hpp
+++ framework/Camera.hpp
@@ -14,9 +14,28 @@ class Camera {
 public:
     Camera();
     Camera(std::string const name, int const fovX);
+    int GetFovX() const {
+        return fovX;
+    }
+
+    glm::vec3 GetN() const {
+        return n;
+    }
+
+    glm::vec3 GetOrigin() const {
+        return origin;
+    }
+
+    glm::vec3 GetUp() const {
+        return up;
+    }
+
 private:
     std::string name;
     int fovX;
+    glm::vec3 origin;
+    glm::vec3 n;
+    glm::vec3 up;
 };
 
 #endif	/* CAMERA_HPP */
