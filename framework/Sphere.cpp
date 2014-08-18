0a64ab317fd6d1b35cf8fedf072f66710dc4e149
= <e-mail@benediktvogler.de>
2014-08-18 22:36:00 +0200

    improve init of vars

--- framework/Sphere.cpp
+++ framework/Sphere.cpp
@@ -10,27 +10,27 @@
 
 std::pair<bool, Ray> Sphere::intersect(Ray const& ray) const {
     glm::vec3 l = ray.origin - center;
-    float a = glm::dot(ray.direction, ray.direction);
-    float b = 2.0f*(glm::dot(l,ray.direction));
-    float c = glm::dot(l,l) - radius*radius;
+    float a{glm::dot(ray.direction, ray.direction)};
+    float b{2.0f*(glm::dot(l,ray.direction))};
+    float c{glm::dot(l,l) - radius*radius};
     
     float det = b*b-4.0f*a*c;
     if (det>=0.0f) {//wenn es min. eine Lösung gibt
-        float t1 = (-b-sqrt(det))/(2.0f*a);
-        float t2 = (-b+sqrt(det))/(2.0f*a);
+        float t1((-b-sqrt(det))/(2.0f*a));
+        float t2((-b+sqrt(det))/(2.0f*a));
         
         if (t1 < t2){//nimm Punkt, welcher näher dran ist
             if (t1<0)
                 return std::make_pair(false, Ray());//keine Lösung
             else{
-                auto p =l-(ray.origin+ray.direction*t1);
+                auto p(l-(ray.origin+ray.direction*t1));
                 return std::make_pair(true, Ray(p,glm::normalize(center-p)));//use t1
             }
         } else{
             if (t2<0)
                 return std::make_pair(false, Ray());//keine Lösung
             else{
-                auto p =l-(ray.origin+ray.direction*t2);
+                auto p(l-(ray.origin+ray.direction*t2));
                 return std::make_pair(true, Ray(p,glm::normalize(center-p)));//use t2
             }
                 
