d808f550af3df6edbd1c3758127d6c8e9c0fc1b3
Cbeed <e-mail@benediktvogler.de>
2014-08-26 15:48:51 +0200

    new output and nogui option

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -1,36 +1,50 @@
 #include <thread>
 #include <renderer.hpp>
 #include <fensterchen.hpp>
+#include <string>
 
-int main(int argc, char* argv[])
-{
-  unsigned const width = 600;
-  unsigned const height = 600;
-  std::string const filename = "./checkerboard.ppm";
+/**
+ * 
+ * @param argc
+ * @param argv first is input path, second ist output path, third to disable window
+ * @return 
+ */
+int main(int argc, char* argv[]) {
+    unsigned const width = 600;
+    unsigned const height = 600;
+    std::string const filename = (argc>2)?argv[2] : "./output.ppm";
 
-  std::cout << "welcome to Raytracer ("<< width<<"x"<<height<<"). Files are stored into:" <<filename<<std::endl;
-  Renderer* app = nullptr;
-  if (argc>0)
-    app = new Renderer(width, height, filename, argv[1]);
-  else
-      app = new Renderer(width, height, filename);
+    std::cout << "welcome to Raytracer ("<< width<<"x"<<height<<"). Files are stored into:" <<filename<<std::endl;
+    Renderer* app = nullptr;
+    if (argc>1)
+        app = new Renderer(width, height, filename, argv[1]);
+    else
+        app = new Renderer(width, height, filename);
 
-  std::thread thr([&app]() { app->render(); });
+    std::thread thr([&app]() { app->render(); });
 
-  Window win(glm::ivec2(width,height));
-
-  while (!win.shouldClose()) {
-    if (win.isKeyPressed(GLFW_KEY_ESCAPE)) {
-      win.stop();
+    bool nogui=false;
+    for (int i = 0; i < argc; i++) {
+        if (string(argv[i]).compare("nogui")==0)
+            nogui=true;
     }
 
-    glDrawPixels( width, height, GL_RGB, GL_FLOAT
-                , app->colorbuffer().data());
+    if (!nogui){
+        Window win(glm::ivec2(width,height));
 
-    win.update();
-  }
+        while (!win.shouldClose()) {
+          if (win.isKeyPressed(GLFW_KEY_ESCAPE)) {
+            win.stop();
+          }
 
-  thr.join();
-  delete(app);
-  return 0;
+          glDrawPixels( width, height, GL_RGB, GL_FLOAT
+                      , app->colorbuffer().data());
+
+          win.update();
+        }
+    }
+    thr.join();
+
+    delete(app);
+    return 0;
 }
