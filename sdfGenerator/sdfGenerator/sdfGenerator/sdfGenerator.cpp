2e50f9a6659a586d96e7cfc33f5c1267f1e11fc4
Shaq <shlapion@yahoo.com>
2014-08-31 10:17:29 +0300

    sdfGenerator
    Generating files

--- sdfGenerator/sdfGenerator/sdfGenerator/sdfGenerator.cpp
+++ sdfGenerator/sdfGenerator/sdfGenerator/sdfGenerator.cpp
@@ -33,13 +33,17 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		double yAxis = i*0.1;
 		ofstream fOut(outputNames[i]);
-		string cameraCmd = "define camera eye 45.0 0 0 " + to_string(i) + " 0 0 -1 0 1 0";
+		string cameraCmd = "define camera eye 65.0 0 0 " + to_string(i) + " 0 0 -1 0 1 0";
 		string outputImg = "render eye " + outputImage[i] + " 600 600";
 		fOut << "define material red 1 0 0 1 0 0 1 0 0 1" << endl
 			<< "define material blue 0 0 1 0 0 1 0 0 1 1" << endl
-			<< "define shape box rbottom 0.0 -7.5 -10.5 2.0 5.0 -6.0 blue" << endl
-			<< "define shape sphere bsphere 0 -6.900000 -10.0 2.0 red" << endl
-			<< "define light diffuse spot1 -500 800 0 0.8 0.8 0.8" << endl
+			<< "define shape box floor   -400 -400 0  400 -410 -8000 blue" << endl
+			<< "define shape box ceiling -400  400 0  400  410 -8000 blue" << endl
+			<< "define shape box left    -400 -400 0 -410  400 -8000 blue" << endl
+			<< "define shape box right    400 -400 0  410  410 -8000 blue" << endl
+			<< "define shape sphere sphere 0 0 -200 30 red" << endl
+			<< "define shape composite root left right floor ceiling sphere" << endl
+			<< "define light ambient amb 0.5 0.5 0.5" << endl
 			<< cameraCmd << endl << outputImg << endl;
 		fOut.close();
 
