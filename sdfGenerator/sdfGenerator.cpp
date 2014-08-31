26f51dd493838fff6aa9c2052364a96a5ea4be44
Cbeed <e-mail@benediktvogler.de>
2014-08-31 20:15:25 +0200

    only creation of files

--- sdfGenerator/sdfGenerator.cpp
+++ sdfGenerator/sdfGenerator.cpp
@@ -1,40 +1,34 @@
 // sdfGenerator.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include <string>
 #include <fstream>
 #include <vector>
 #include <array>
 #include <iostream>
-#include <process.h>
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int main(int argc, char* argv[]) {
+	const int imagecount = 120;
+	array<string, imagecount> outputNames;
 
-	array<string, 101> outputNames;
-
-	for (int  i = 1; i <= 100; i++)
-	{
-		outputNames[i] = "file" + to_string(i + 140) + ".txt";
+	for (int  i = 0; i < imagecount; ++i) {
+		outputNames[i] = "file" + to_string(i) + ".sdf";
 		
 		cout << outputNames[i] << endl;
 	}
 
 
-	array <string, 141> outputImage;
+	array <string, imagecount> outputImage;
 
-	for (int i = 1; i <=140; i++)
-	{
-		outputImage[i] = "picture1" + to_string(i + 140) + ".ppm";
+	for (int i = 0; i <imagecount; ++i){
+		outputImage[i] = "picture" + to_string(i) + ".ppm";
 	}
 
-	for (int i = 1; i <= 100; i++)
-	{
+	for (int i = 0; i < imagecount; ++i){
 		double yAxis = i*0.1;
-		ofstream fOut(outputNames[i]);
+		ofstream fOut("output/"+outputNames[i]);
 		string cameraCmd = "define camera eye 65.0 0 0 " + to_string(i) + " 0 0 -1 0 1 0";
 		string outputImg = "render eye " + outputImage[i] + " 600 600";
 		fOut << "define material red 1 0 0 1 0 0 1 0 0 1" << endl
@@ -49,17 +43,6 @@ int _tmain(int argc, _TCHAR* argv[])
 			<< cameraCmd << endl << outputImg << endl;
 		fOut.close();
 
-	}
-
-
-	puts("Doing something");
-
-	for (int i = 141; i < 143; i++)
-	{
-		char buffer[50];
-		sprintf_s(buffer, "file%d.txt", i);
-		_spawnl(P_WAIT, "raytracer.exe", "gridscene.sdf", buffer, "Arg2", NULL); //renders each file seperately, must close the generated image by hand
-		//_spawnl(P_NOWAIT, "raytracer.exe", "gridscene.sdf", buffer, "Arg2", NULL); renders every file at the same time
 	}
 
 	return 0;
